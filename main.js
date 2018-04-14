e28c0be527a3c3a2b1b6199836709d576c00e13b
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-04-14 13:40:00 -0500

    inquier prompt

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
    host: "localhost",
@@ -12,4 +13,23 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
    if(err) throw(err);
    console.log(`connected as id ${connection.threadId}`);
+});
+
+
+inquirer.prompt([
+  {
+    name: "answer",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["Post an item", "Bid on item"]
+  },
+]).then(function(answer) {
+    console.log(answer);
+
+    if (answer == "Post an item") {
+        console.log("post something");
+    }
+    else {
+        console.log("do the bid thing");
+    }
 });
